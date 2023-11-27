c5f3041f5fe4d4e17fb689946f7ae86bd1393911
Portablefire22 <Barnowlsarecool@gmail.com>
2023-11-27 11:25:02 +0000

    Fixed a typo in findMemoryType condition

--- main.cpp
+++ main.cpp
@@ -703,9 +703,8 @@ chunk::chunk testChunk = chunk::chunk({0,0,0});
     uint32_t mainApp::findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties) {
         VkPhysicalDeviceMemoryProperties memProperties;
         vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties);
-
         for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
-            if ((typeFilter & (1 << 1)) && (memProperties.memoryTypes[i].propertyFlags & properties) == properties) {
+            if ((typeFilter & (1 << i)) && (memProperties.memoryTypes[i].propertyFlags & properties) == properties) {
                 return i;
             }
         }
