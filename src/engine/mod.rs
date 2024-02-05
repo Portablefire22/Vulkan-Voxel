faa41f499f719136d29936e31eaece87a0f27d0b
Portablefire22 <barnowlsarecool@gmail.com>
2024-02-05 20:37:03 +0000

    Slowly figuring out how the crate system works for local files

---
+++ src/engine/mod.rs
@@ -0,0 +1,7 @@
+pub mod vk_descriptors;
+pub mod vk_engine;
+pub mod vk_images;
+pub mod vk_initializers;
+pub mod vk_loader;
+pub mod vk_pipelines;
+pub mod vk_types;
--- src/main.rs
+++ src/main.rs
@@ -7,72 +7,12 @@ use vulkano::memory::allocator::StandardMemoryAllocator;
 use vulkano::memory::allocator::{AllocationCreateInfo, MemoryTypeFilter};
 use vulkano::VulkanLibrary;
 
+// So this is going to be an attempt to re-write the updated 'vkguide.dev' project in Rust.
+// The previous iteration of this project used the previous version of the guide, so I thought
+// that I should probably use the new version of the guide to get the best possible framework.
+
+mod engine;
+
 fn main() {
-    let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
-    let instance =
-        Instance::new(library, InstanceCreateInfo::default()).expect("failed to create instance");
-
-    let physical_device = instance
-        .enumerate_physical_devices()
-        .expect("could not enumerate devices")
-        .next()
-        .expect("no devices available, you probably don't have a Vulkan capable graphics card :(");
-
-    for family in physical_device.queue_family_properties() {
-        println!(
-            "Found a queue family with {:?} queue(s)",
-            family.queue_count
-        );
-    }
-
-    let queue_family_index = physical_device
-        .queue_family_properties()
-        .iter()
-        .enumerate()
-        .position(|(_queue_family_index, queue_family_properties)| {
-            queue_family_properties
-                .queue_flags
-                .contains(QueueFlags::GRAPHICS)
-        })
-        .expect("couldn't find a graphical queue family") as u32;
-
-    let (device, mut queues) = Device::new(
-        physical_device,
-        DeviceCreateInfo {
-            // here we pass the desired queue family to use by index
-            queue_create_infos: vec![QueueCreateInfo {
-                queue_family_index,
-                ..Default::default()
-            }],
-            ..Default::default()
-        },
-    )
-    .expect("failed to create device");
-
-    let queue = queues.next().unwrap();
-
-    let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
-
-    let iter = (0..128).map(|_| 4u8);
-    let buffer = Buffer::from_iter(
-        memory_allocator.clone(),
-        BufferCreateInfo {
-            usage: BufferUsage::UNIFORM_BUFFER,
-            ..Default::default()
-        },
-        AllocationCreateInfo {
-            memory_type_filter: MemoryTypeFilter::PREFER_DEVICE
-                | MemoryTypeFilter::HOST_SEQUENTIAL_WRITE,
-            ..Default::default()
-        },
-        iter,
-    )
-    .unwrap();
-
-    // Read the buffer
-    // read() gives shared access
-    // write() is exclusive access
-    let mut content = buffer.write().unwrap();
-    content[12] = 83;
-    content[7] = 3;
+    engine::vk_engine::init();
 }
